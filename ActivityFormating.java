79ecf17689187135442cfd0bbe89fb721b14204a
bfoley05 <142286051+bfoley05@users.noreply.github.com>
2023-10-10 23:41:03 -0700

    Update ActivityFormating.java

--- ActivityFormating.java
+++ ActivityFormating.java
@@ -1,16 +1,28 @@
 package Garmin;
 import java.util.List;
+/**
+ * The purpose of this class is to format one line of data into
+ * a better format that is more readable.
+ * @author Brandon Foley
+ * @version 1.0
+ */
 
 
 public class ActivityFormating {
     List<String> dataLine;
-    public ActivityFormating(){
-        System.out.println("test failed");
-    }
+
+    /**
+     * Overloaded constructor that takes in the single line
+     * of data in a List and instantiates the list for the class
+     * @param dataLine type List<String>
+     */
     public ActivityFormating(List<String> dataLine){
         this.dataLine = dataLine;
     }
-
+    /**
+     * Method used to print out all the data in a clean
+     * and organized manner
+     */
     public void printFormating(){
         System.out.println("---------------------------------------------------------------------");
         System.out.println(dataLine.get(0) + " " + dataLine.get(3) + ": ");
