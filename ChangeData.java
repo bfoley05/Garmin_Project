f1da7b495f6e803f4ff760cf505b171c51653141
bfoley05 <142286051+bfoley05@users.noreply.github.com>
2023-10-11 15:32:28 -0700

    Create ChangeData.java

---
+++ ChangeData.java
@@ -0,0 +1,37 @@
+package Garmin;
+import java.util.List;
+
+public class ChangeData {
+    static List<List<String>> data;
+    public ChangeData(List<List<String>> d){
+        data = d;
+        editTimes();
+        editVariables();
+    }
+
+    public static void editTimes(){
+        for(int i = 1; i < data.size(); i++){
+            if(data.get(i).get(6).indexOf(".") != -1){
+                String time = data.get(i).get(6).substring(0,data.get(i).get(6).indexOf("."));
+                data.get(i).set(6,time);
+            }
+        }
+    }
+
+    public static void editVariables(){
+        String temp;
+        for(int i = 1; i<data.size(); i++){
+            for(int j = 0; j < data.get(0).size(); j++){
+                System.out.println(data.get(12).get(5));
+                temp = data.get(i).get(j).replace("\"","");
+                data.get(i).set(j, temp);
+                temp = data.get(i).get(j).replace(":",".");
+                data.get(i).set(j, temp);
+            }
+        }
+    }
+
+    public List<List<String>> getList(){
+        return data;
+    }
+}
